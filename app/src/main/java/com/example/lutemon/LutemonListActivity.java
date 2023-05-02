d62fa64d76c3fcfa77a975f2db61a0efa4577ac7
Samuli Saarinen <samuli.jas@gmail.com>
2023-05-02 14:44:11 +0300

    as

--- app/src/main/java/com/example/lutemon/LutemonListActivity.java
+++ app/src/main/java/com/example/lutemon/LutemonListActivity.java
@@ -8,17 +8,14 @@ import android.os.Bundle;
 
 public class LutemonListActivity extends AppCompatActivity {
 
-    private Player player;
-    private RecyclerView recyclerView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rocket_list);
 
-        player = Player.getInstance();
+        Player player = Player.getInstance();
 
-        recyclerView = findViewById(R.id.rvRocketList);
+        RecyclerView recyclerView = findViewById(R.id.rvRocketList);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new LutemonListAdapter(getApplicationContext(), player.getLutemons()));
